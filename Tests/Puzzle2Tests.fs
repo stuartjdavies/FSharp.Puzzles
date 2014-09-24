ff9aebf3dd0debc29fdaa04ad3f0b3281595f13a
stuartjdavies <stuart_j_davies@hotmail.com>
2014-09-24 11:08:39 +1000

    Changed some names

--- Tests/Puzzle2Tests.fs
+++ Tests/Puzzle2Tests.fs
@@ -9,7 +9,7 @@ open FsUnit.MsTest
 [<TestClass>]
 type ``Puzzle 2: Determine triangle type from the lengths of the sides``() =
     [<TestMethod>]
-    member this.``Puzzle 2: try different combinations of triangle lengths``() =       
+    member this.``Puzzle 2: Test various combinations of side lengths``() =       
              let input = [[1.0F; 1.0F; 1.0F];
                           [2.2F; 2.2F; 2.2F];
                           [1.0F; 2.0F; 1.0F];
