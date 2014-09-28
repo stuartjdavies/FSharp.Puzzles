d09a6f3cdcc3059587168a1ed7f200af349031a5
stuartjdavies <stuart_j_davies@hotmail.com>
2014-09-29 09:37:32 +1000

    Added small puzzles

---
+++ Solution/SmallPuzzles.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp.Puzzles.Solutions
+
+module ``List of small puzzles`` =
+     let Sum(xs) = xs |> List.reduce (+)
+
+     let Multiply(xs) = xs |> List.reduce (*)
+
+     // To be continued ...
+        
---
+++ Tests/SmallPuzzlesTests.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharp.Puzzles.Tests
+
+open System
+open FSharp.Puzzles.Solutions
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
+open FsUnit.MsTest
+
+[<TestClass>]
+type ``Small Puzzles: Test case for a list of small puzzles``() =
+    [<TestMethod>]   
+    member this.``Sum: Verify the sum of a list of numbers``() =                           
+            ``List of small puzzles``.Sum([1;2;3;4]) |> should equal 10
+
+    [<TestMethod>]   
+    member this.``Multiplication: Verify the multiplication of a list of numbers``() =                           
+            ``List of small puzzles``.Multiply([1;2;3;4]) |> should equal 24
+
+    // To be continued ...
+
+
