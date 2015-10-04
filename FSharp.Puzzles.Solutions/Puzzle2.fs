6170c41ab26e29a868264b35cf30da6c3b20156a
stuartjdavies <stuart_j_davies@hotmail.com>
2015-10-04 22:30:41 +1100

    Added isScalene Method back in

--- FSharp.Puzzles.Solutions/Puzzle2.fs
+++ FSharp.Puzzles.Solutions/Puzzle2.fs
@@ -1,19 +1,15 @@
 ﻿module FSharp.Puzzles.Solutions.Puzzle2
 
-type TriangleType =
-        | Scalene 
-        | Isosceles
-        | Equilateral
-        | Error   
+type TriangleType = | Scalene | Isosceles | Equilateral | Unknown   
 
 let private isEquilateral len1 len2 len3 = (len1=len2) && (len3 = len2) 
-let private isIsosceles len1 len2 len3 = (len1 = len3 && len1 <> len2) || 
-                                            (len2 = len3 && len1 <> len2) || 
-                                            (len1 = len2 && len1 <> len3)
-//let private isScalene len1 len2 len3 = (len1 <> len2) && (len1 <> len3) && (len2 <> len3)                                        
+let private isIsosceles len1 len2 len3 = (len1 = len3 && len1 <> len2) || (len2 = len3 && len1 <> len2) || (len1 = len2 && len1 <> len3)
+let private isScalene len1 len2 len3 = (len1 <> len2) && (len1 <> len3) && (len2 <> len3)  
+                                      
 let getTriangleType(len1,len2,len3) =                               
         if isEquilateral len1 len2 len3 then Equilateral
         else if isIsosceles len1 len2 len3 then Isosceles
-        else Scalene
+        else if isScalene len1 len2 len3  then Scalene 
+        else Unknown
          
         
