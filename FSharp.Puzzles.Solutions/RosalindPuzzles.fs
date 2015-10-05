b940b0a34e36671dfb4840479d965f7211eda05c
stuartjdavies <stuart_j_davies@hotmail.com>
2015-10-05 19:56:18 +1100

    Added rosalind puzzles

--- FSharp.Puzzles.Solutions/RosalindPuzzles.fs
+++ FSharp.Puzzles.Solutions/RosalindPuzzles.fs
@@ -7,23 +7,15 @@ open System.Text.RegularExpressions
 type DnaString = string
 type RnaString = string
 
-//
 //  Title: Counting DNA Nucleotides
-//  Detals: http://rosalind.info/problems/dna/
-//
-let solveProblemWithId_DNA (s : DnaString) = (" ", [| 'A'; 'C'; 'G'; 'T' |] |> Array.map(fun k -> s.Count(fun sym -> sym = k))) |> String.Join
+let solveDNA (s : DnaString) = (" ", [| 'A'; 'C'; 'G'; 'T' |] |> Array.map(fun k -> s.Count(fun sym -> sym = k))) |> String.Join
                     
-//
 // Title Transcribing DNA into RNA
-// Details: http://rosalind.info/problems/rna/
-// 
-let solveProblemWithId_RNA (s : DnaString) = new DnaString [| for k in s -> if k = 'T' then 'U' else k |]
+let solveRNA (s : DnaString) = new DnaString [| for k in s -> if k = 'T' then 'U' else k |]
 
-//
+
 // Title: Complementing a Strand of DNA 
-// Details: http://rosalind.info/problems/revc/
-// 
-let solveProblemWithId_REVC (s : DnaString) = 
+let solveREVC (s : DnaString) = 
            let revComplement c = match c with 
                                  | 'A' -> 'T'
                                  | 'T' -> 'A'
@@ -32,37 +24,25 @@ let solveProblemWithId_REVC (s : DnaString) =
                                  | _ -> c
            s |> Seq.toArray |> Array.rev |> Array.map revComplement |> (fun xs -> new string(xs))
  
-//
 // Title: Rabbits and Recurrence Relations
-// Details: http://rosalind.info/problems/fib/
-// 
-let solveProblemWithId_FIB (n : int) (k : int) = String.Empty
+let solveFIB (n : int) (k : int) = String.Empty
     
-//
 // Title: Computing GC Content      
-// Details: http://rosalind.info/problems/gc/
 // TODO: Absolute Error
-//
-let sovleProblemWithId_GC (ss : DnaString array)=       
+let sovleGC (ss : DnaString array)=       
         let matches = ss |> Seq.fold(fun acc s -> acc + (float (s.Count(fun item -> item='C' || item='G')))) 0.0
         let totalItems = ss |> Seq.fold(fun acc s -> acc + (float (s.Length))) 0.0
         (matches / totalItems) * 100.00
               
-// 
 // Title: Counting Point mutations
-// Details: http://rosalind.info/problems/hamm/
-//
-let solveProblemWithId_HAMM (s : DnaString) (t : DnaString) = 
+let solveHAMM (s : DnaString) (t : DnaString) = 
            (s, t) ||> Seq.map2 (fun sItem tItem -> if sItem <> tItem then 1 else 0) |> Seq.sum
 
 
 let solveProblemWithId_IPRB = String.Empty
 
-//
 // Title: Translating RNA into Protein
-// Details: http://rosalind.info/problems/prot/                    
-// 
-let solveProblemWithId_PROT (s : RnaString) = 
+let solvePROT (s : RnaString) = 
           let rnaCondonTable = [| "UUU","F";"CUU","L";"AUU","I";"GUU","V"
                                   "UUC","F";"CUC","L";"AUC","I";"GUC","V"
                                   "UUA","L";"CUA","L";"AUA","I";"GUA","V"
@@ -87,11 +67,8 @@ let solveProblemWithId_PROT (s : RnaString) =
                     (v.[0]::acc) 
           aux List.Empty s |> List.rev |> List.toArray |> (fun items -> new string (items))          
 
-// 
 // Title: Finding a Motif in DNA
-// Details: http://rosalind.info/problems/subs/                
-//
-let solveProblemWithId_SUBS (s : DnaString) (t : DnaString) = 
+let solveSUBS (s : DnaString) (t : DnaString) = 
         let rec aux acc (index : int) =
             let index = s.IndexOf(t, index)
             if index < 0 then 
@@ -100,14 +77,10 @@ let solveProblemWithId_SUBS (s : DnaString) (t : DnaString) =
               aux ((index + 1)::acc) (index + 1) 
         aux [] 0 |> List.rev                     
            
-let solveProblemWithId_CONS (s : RnaString) = String.Empty
-        
-        
-//
+let solveCONS (s : RnaString) = String.Empty
+           
 // Title: Finding a Shared Motif 
-// Details: http://rosalind.info/problems/lcsm/
-//
-let solveProblemWithId_LCSM (s : DnaString) = 
+let solveLCSM (s : DnaString) = 
            Regex(@"(.+)\1", RegexOptions.IgnoreCase).Match(s) 
 
            //let rec findMatch startIndex endIndex = 
@@ -117,6 +90,6 @@ let solveProblemWithId_LCSM (s : DnaString) =
            //                 if startIndex = endIndex then String.Empty else findMatch startIndex (endIndex - 1)
            //[| 0 .. (s.Length - 1) |] |> Array.map(fun i -> findMatch i (s.Length - 1))                  
                                
-solveProblemWithId_LCSM "aa12bbbbb2bbbbb4aaa225" |> (fun m -> m.Groups.[2])                               
+// solveProblemWithId_LCSM "aa12bbbbb2bbbbb4aaa225" |> (fun m -> m.Groups.[2])                               
                            
           
